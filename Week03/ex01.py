c26becb429a87159bb0ceb35b1bff6640c3ac462
walson <298100732@qq.com>
2020-10-18 23:13:55 +0800

    new task

---
+++ Week03/ex01.py
@@ -0,0 +1,16 @@
+# 示例代码
+import threading
+class DiningPhilosophers:
+   def __init__(self):
+   pass
+# philosopher 哲学家的编号。
+# pickLeftFork 和 pickRightFork 表示拿起左边或右边的叉子。
+# eat 表示吃面。
+# putLeftFork 和 putRightFork 表示放下左边或右边的叉子。
+   def wantsToEat(self,
+      philosopher,
+      pickLeftFork(),
+      pickRightFork(),
+      eat(),
+      putLeftFork(),
+      putRightFork())
